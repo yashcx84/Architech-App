03d660623ff21a6354a849e5bbbc38164dce7df7
sarjeet221 <sarjeetkumawat221@gmail.com>
2024-04-30 13:17:23 +0530

    services screen added

--- src/index.js
+++ src/index.js
@@ -3,23 +3,11 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-import Main from './Main';
-import Main2 from './Main2';
-import Services from './screens/Services';
-=======
->>>>>>> 56f5d66fed7bc045677828da299dc8e4db67a0f3
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
-    <Main/>
-    <Main2 />
-    <Services />
-=======
->>>>>>> 56f5d66fed7bc045677828da299dc8e4db67a0f3
   </React.StrictMode>
 );
 
